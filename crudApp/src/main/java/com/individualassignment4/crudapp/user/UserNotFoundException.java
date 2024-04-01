113f8a5082d05a510ab23b0dc31cf17cbe0b6d7b
CavsUNCGaccount <156218585+CavsUNCGaccount@users.noreply.github.com>
2024-04-01 18:01:17 -0400

    Delete crudApp directory

--- crudApp/src/main/java/com/individualassignment4/crudapp/user/UserNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.individualassignment4.crudapp.user;
-
-public class UserNotFoundException extends Throwable{
-    public UserNotFoundException(String message){
-        super(message);
-    }
-
-}
--- crudApp/src/main/java/com/individualassignment4/crudapp/user/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.individualassignment4.crudapp.user;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface UserRepository extends CrudRepository<User, Integer> {
-
-
-}
