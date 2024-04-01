e7b1189e68de2fbb820525d2585b06b7876aa34c
CavsUNCGaccount <156218585+CavsUNCGaccount@users.noreply.github.com>
2024-04-01 14:41:02 +0000

    Add files via upload

---
+++ crudApp/src/main/java/com/individualassignment4/crudapp/user/UserRepository.java
@@ -0,0 +1,8 @@
+package com.individualassignment4.crudapp.user;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserRepository extends CrudRepository<User, Integer> {
+
+
+}
