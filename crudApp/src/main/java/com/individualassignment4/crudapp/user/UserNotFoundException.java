338bdcc6a18408de0ac2f78c07930b6cc2af8fa8
CavsUNCGaccount <156218585+CavsUNCGaccount@users.noreply.github.com>
2024-04-01 22:03:03 +0000

    Crud App
    Uses a database called usersdb with a users table.

---
+++ crudApp/src/main/java/com/individualassignment4/crudapp/user/UserNotFoundException.java
@@ -0,0 +1,8 @@
+package com.individualassignment4.crudapp.user;
+
+public class UserNotFoundException extends Throwable{
+    public UserNotFoundException(String message){
+        super(message);
+    }
+
+}
---
+++ crudApp/src/main/java/com/individualassignment4/crudapp/user/UserRepository.java
@@ -0,0 +1,9 @@
+package com.individualassignment4.crudapp.user;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserRepository extends CrudRepository<User, Integer> {
+    public long countById(Integer id);
+
+
+}
